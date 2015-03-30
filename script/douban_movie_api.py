6a6cba02b3d83a54311463cd2426a370cac998b0
yinyu <986450042@qq.com>
2015-03-30 21:13:06 +0800

    add bs4 analysic downlink html

--- script/douban_movie_api.py
+++ script/douban_movie_api.py
@@ -20,6 +20,9 @@ import sys
 import os
 import logging
 from image_load import *
+from bs4 import BeautifulSoup
+import requests
+import urllib
 
 
 MONGO_HOST = "127.0.0.1"
@@ -28,6 +31,8 @@ MONGO_DATABASE = "dm"
 MONGO_COLLECTION = "movie"
 
 DOUBAN_BASIC_URL = "https://api.douban.com/v2/movie/subject/%s"
+DOWNLINK_URL = "http://www.btbook.net/search/"
+
 
 def mongo_init():
     """
@@ -67,23 +72,67 @@ def check_requests_status(status):
         logging.error("未知错误")
         return False
 
+def downlink_to_dict(title, flag=1):
+    """
+    bs4 正则解析html，获取下载链接
+    """
+    import ipdb
+    ipdb.set_trace()
+    url = DOWNLINK_URL + urllib.quote(title)
+    if flag == 1:
+        url += ".html"
+    else:
+        url += "/2-1.html"
+    html = requests.get(url)
+    h = BeautifulSoup(html.content)
+    lists = h.find_all("div", class_="search-item")
+    for list in lists:
+        pass
+
+    
+
+
 def douban_to_dict(id):
     """
     通过豆瓣的电影id，读取json，然后返回字典
     """
     contains = requests.get(DOUBAN_BASIC_URL %(id))
-    time.sleep(1.5)
     if not check_requests_status(contains.status_code):
         return {}
     text = contains.text
     text = json.loads(text)
-    movies = {}
-
-
-
-
-
+    movie = {}
+    movie["rating"] = text["rating"]
+    movie["year"] = text["year"]
+    movie["origin_image"] = text["images"] 
+    movie["image"] = {}
+    for image in text["images"]:
+        image_url = text["images"][image]
+        url = store_url(image_url)
+        if url:
+            movie["image"][image] = url
+    movie["id"] = text["id"]
+    movie["title"] = text["title"]
+    movie["category"] = text["genres"]
+    movie["countries"] = text["countries"]
+    movie["casts"] = []
+    for cast in text["casts"]:
+        movie["casts"].append(cast["name"])
     
+    movie["original_title"] = text["original_title"]
+    movie["summary"] = text["summary"]
+    movie["subtype"] = text["subtype"]
+    movie["directors"] = []
+    for director in text["directors"]:
+        movie["directors"].append(director["name"])
+    movie["aka"] = text["aka"]
+    time.sleep(1.5)
+    import ipdb
+    ipdb.set_trace()
+    print movie
+
     
 if __name__ == "__main__":
-    douban_to_dict(1764796)
+    #douban_to_dict(1764796)
+    title = "超能陆战队"
+    downlink_to_dict(title)
