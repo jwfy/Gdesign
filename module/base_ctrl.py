70395b1010deace30569bf8e0b92c1843876307f
yinyu <986450042@qq.com>
2015-03-31 21:05:44 +0800

    finished spider downlink from html and store in mongo

---
+++ module/base_ctrl.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# author: jwfy
+# time: 2015-03-31
+# e-mail: jwfy0902@foxmail.com
+
+import time
+import hashlib
+
+def unicode_to_str(u):
+    """
+    unicode to str
+    """
+    return u.encode("utf-8")
+
+def unicode_to_md5(u):
+    """
+    unicode to str to md5
+    """
+    u = unicode_to_str(u)
+    return hashlib.md5(u).hexdigest()
+
+
--- script/douban_movie_api.py
+++ script/douban_movie_api.py
@@ -23,7 +23,9 @@ from image_load import *
 from bs4 import BeautifulSoup
 import requests
 import urllib
-
+import ipdb
+sys.path.insert(0, "../module/")
+from base_ctrl import *
 
 MONGO_HOST = "127.0.0.1"
 MONGO_PORT = 27017
@@ -33,6 +35,7 @@ MONGO_COLLECTION = "movie"
 DOUBAN_BASIC_URL = "https://api.douban.com/v2/movie/subject/%s"
 DOWNLINK_URL = "http://www.btbook.net/search/"
 
+_Collection = None
 
 def mongo_init():
     """
@@ -41,7 +44,8 @@ def mongo_init():
     client = MongoClient(MONGO_HOST, MONGO_PORT)
     database = client[MONGO_DATABASE]
     collection = database[MONGO_COLLECTION]
-    return collection
+    global _Collection
+    _Collection = collection
 
 def check_requests_status(status):
     """
@@ -72,27 +76,41 @@ def check_requests_status(status):
         logging.error("未知错误")
         return False
 
-def downlink_to_dict(title, flag=1):
+def downlink_to_list(title, flag=1):
     """
     bs4 正则解析html，获取下载链接
     """
-    import ipdb
-    ipdb.set_trace()
     url = DOWNLINK_URL + urllib.quote(title)
     if flag == 1:
         url += ".html"
     else:
         url += "/2-1.html"
     html = requests.get(url)
+    if not check_requests_status(html.status_code):
+        return {}
     h = BeautifulSoup(html.content)
     lists = h.find_all("div", class_="search-item")
+    res_list = []
     for list in lists:
-        pass
+        rst_dict = {}
+        
+        b = list.find_all("b")
+        rst_dict["size"] = unicode_to_str(b[1].string)
+        rst_dict["hot"] = int(unicode_to_str(b[2].string))
+        
+        if rst_dict["hot"] < 10:
+            continue
 
+        down_link_dict = {}
+        down_link = list.find_all("a", class_="download")
+        for link in down_link:
+            down_link_dict[link.string] = link.get("href")
+
+        rst_dict["link"] = down_link_dict
+        res_list.append(rst_dict)
+    return res_list
     
-
-
-def douban_to_dict(id):
+def douban_to_mongo(id):
     """
     通过豆瓣的电影id，读取json，然后返回字典
     """
@@ -102,37 +120,57 @@ def douban_to_dict(id):
     text = contains.text
     text = json.loads(text)
     movie = {}
-    movie["rating"] = text["rating"]
-    movie["year"] = text["year"]
-    movie["origin_image"] = text["images"] 
+    movie["rating"] = text.get("rating")
+    movie["year"] = text.get("year")
+    movie["origin_image"] = text.get("images")
     movie["image"] = {}
-    for image in text["images"]:
-        image_url = text["images"][image]
-        url = store_url(image_url)
+    for k, v in text.get("images", "{}").iteritems():
+        image_url = v
+        url = store_url(image_url, "douban-"+k)
+        time.sleep(1.0)
         if url:
-            movie["image"][image] = url
-    movie["id"] = text["id"]
-    movie["title"] = text["title"]
-    movie["category"] = text["genres"]
-    movie["countries"] = text["countries"]
+            movie["image"][k] = url
+    movie["id"] = text.get("id")
+    movie["title"] = text.get("title")
+    movie["category"] = text.get("genres")
+    movie["countries"] = text.get("countries")
     movie["casts"] = []
-    for cast in text["casts"]:
+    for cast in text.get("casts"):
         movie["casts"].append(cast["name"])
     
-    movie["original_title"] = text["original_title"]
-    movie["summary"] = text["summary"]
-    movie["subtype"] = text["subtype"]
+    movie["original_title"] = text.get("original_title")
+    movie["summary"] = text.get("summary")
+    movie["subtype"] = text.get("subtype")
     movie["directors"] = []
-    for director in text["directors"]:
+    for director in text.get("directors"):
         movie["directors"].append(director["name"])
-    movie["aka"] = text["aka"]
-    time.sleep(1.5)
-    import ipdb
-    ipdb.set_trace()
-    print movie
-
+    movie["aka"] = text.get("aka")
     
+    movie["down_link"] = downlink_to_list(unicode_to_str(movie["title"]))
+
+    _id = write_to_mongo(movie)
+    return _id
+
+def write_to_mongo(movie_dict):
+    """
+    dict store to mongo
+    """
+    if not _Collection:
+        mongo_init()
+    id = _Collection.insert(movie_dict).str
+    return id
+
+def main():
+    id = '1764796'
+    files = open('./douban.txt', 'wb')
+    _id = douban_to_mongo(id)
+    print "%s\n" %(_id)
+    files.write("存入数据库id=%s\n" %(_id))
+    files.close()
+    
+
 if __name__ == "__main__":
+    main()
     #douban_to_dict(1764796)
-    title = "超能陆战队"
-    downlink_to_dict(title)
+    #title = "超能陆战队"
+    #downlink_to_list(title)
